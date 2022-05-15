5599f9bba1fa109cbf643fa0c34f3f287479d9ed
jheave <jimenezjheave123@gmail.com>
2022-05-15 22:37:57 +0800

    optimize code

--- app/src/main/java/com/example/foodapp/MainActivity.java
+++ app/src/main/java/com/example/foodapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.foodapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.models.SlideModel;
@@ -11,19 +13,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout linearLayout;
+    String[] imagesUri= new String[]{
+            "https://picsum.photos/id/237/200/30e",
+            "https://picsum.photos/seed/picsum/200/300",
+            "https://picsum.photos/200/300?grayscale"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        List<SlideModel> imageContent=new ArrayList<>();
-        imageContent.add(new SlideModel(  "https://picsum.photos/id/237/200/30e"));
-        imageContent.add(new SlideModel(  "https://picsum.photos/seed/picsum/200/300"));
-        imageContent.add(new SlideModel(  "https://picsum.photos/200/300?grayscale"));
-        imageContent.add(new SlideModel(  "https://picsum.photos/id/237/200/300"));
-
+        List<SlideModel> imageContent = new ArrayList<>();
         ImageSlider imageSlider = findViewById(R.id.image_slider);
+
+        linearLayout = findViewById(R.id.linear_layout);
+        for (String s : imagesUri) {
+            imageContent.add(new SlideModel(s));
+        }
+
         imageSlider.setImageList(imageContent, true);
     }
 }
